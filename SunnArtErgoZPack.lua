fbc5ab06d67f44193ed8ae9e57683caa3b5a0404
riftbit [ErgoZ] <ergozru@gmail.com>
2020-12-05 23:02:49 +0300

    Added Warcraft III - Human UI by ErgoZ

--- SunnArtErgoZPack.lua
+++ SunnArtErgoZPack.lua
@@ -1,7 +1,12 @@
 if not SunnArtPack then SunnArtPack = {theme = {}, overlap = {}, panels = {}, length = {}} end
+
 SunnArtPack.theme["SunnArtErgoZPack\\wc3_horde\\wc3_horde-"] = "Warcraft III - Horde"
 SunnArtPack.overlap["SunnArtErgoZPack\\wc3_horde\\wc3_horde-"] = 28.516
 SunnArtPack.panels["SunnArtErgoZPack\\wc3_horde\\wc3_horde-"] = 2
+
+SunnArtPack.theme["SunnArtErgoZPack\\wc3_human_ui_ergoz\\wc3_human_ui_ergoz-"] = "Warcraft III - Human UI by ErgoZ"
+SunnArtPack.overlap["SunnArtErgoZPack\\wc3_human_ui_ergoz\\wc3_human_ui_ergoz-"] = 29.687
+SunnArtPack.panels["SunnArtErgoZPack\\wc3_human_ui_ergoz\\wc3_human_ui_ergoz-"] = 4
 
 SunnArtPack.theme["SunnArtErgoZPack\\wc3_human_ui2\\wc3_human_ui2-"] = "Warcraft III - Human UI"
 SunnArtPack.overlap["SunnArtErgoZPack\\wc3_human_ui2\\wc3_human_ui2-"] = 29.687
@@ -18,7 +23,3 @@ SunnArtPack.panels["SunnArtErgoZPack\\wc3_orc_ui\\wc3_orc_ui-"] = 4
 SunnArtPack.theme["SunnArtErgoZPack\\wc3_undead_ui\\wc3_undead_ui-"] = "Warcraft III - Undead UI"
 SunnArtPack.overlap["SunnArtErgoZPack\\wc3_undead_ui\\wc3_undead_ui-"] = 29.687
 SunnArtPack.panels["SunnArtErgoZPack\\wc3_undead_ui\\wc3_undead_ui-"] = 4
-
-SunnArtPack.theme["SunnArtErgoZPack\\wc3_decorative\\wc3_decorative-"] = "Warcraft III - Decorative"
-SunnArtPack.overlap["SunnArtErgoZPack\\wc3_decorative\\wc3_decorative-"] = 0
-SunnArtPack.panels["SunnArtErgoZPack\\wc3_decorative\\wc3_decorative-"] = 4
